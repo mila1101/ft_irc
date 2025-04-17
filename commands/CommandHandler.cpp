fef2ee2c537ae6a97e9b66370a363ebb6d75571b
Siria <siria_miren@icloud.com>
2025-04-17 16:35:25 +0200

    cmdnick update

--- commands/CommandHandler.cpp
+++ commands/CommandHandler.cpp
@@ -6,14 +6,13 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 00:33:36 by eahn              #+#    #+#             */
-/*   Updated: 2025/04/17 15:03:10 by smiranda         ###   ########.fr       */
+/*   Updated: 2025/04/17 16:18:34 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CommandHandler.hpp"
 #include "../utils/Logger.hpp"
 #include <iostream>
-#include <sys/socket.h> // for send()
 
 CommandHandler::CommandHandler()
 {
@@ -85,25 +84,66 @@ void CommandHandler::dispatch(int clientFd, const ParsedCommand& cmd)
 //     send(fd, msg.c_str(), msg.length(), 0);  // Send success response
 // }
 
-void CommandHandler::cmdNick(int fd, const std::vector<std::string>& params)
+void CommandHandler::cmdNick(int clientFd, const std::vector<std::string>& params)
 {
-    // Check if parameter is empty
+    std::map<int, Client>& clients = server_.getClients(); //getClients tbd
+    const std::map<std::string, Channel>& channels = server_.getChannels(); //getChannels tbd
+
     if (params.empty())
     {
-        Logger::warning("NICK: no nickname provided from fd=" + std::to_string(fd));
-        std::string err = "431 :No nickname given\r\n";
-        send(fd, err.c_str(), err.length(), 0);
+        Logger::warning("NICK: no nickname provided from fd=" + std::to_string(clientFd));
+        server_.msgClient(clientFd, ERR_NONICKNAMEGIVEN(server_.getIP())); //getIP tbd
         return;
     }
-    std::regex pattern("^[a-zA-Z][a-zA-Z0-9\\-_]{0,8}$");
     std::string newNick = params[0];
+    std::regex pattern("^[a-zA-Z][a-zA-Z0-9\\-_]{0,8}$");
+
     if (!std::regex_match(newNick, pattern))
     {
-        
+        server_.msgClient(clientFd, ERR_ERRONEUSNICKNAME(server_.getIP(), newNick));
+        return;
+    }
+    
+    for (const auto& pair : clients)
+    {
+        if (pair.second.getNickname() == newNick && pair.first != clientFd)
+        {
+            server_.msgClient(clientFd, ERR_NICKNAMEINUSE(server_.getIP(), newNick));
+            return;
+        }
     }
 
+    Client& client = clients[clientFd];
+    std::string oldNick = client.getNickName();
 
-} 
+    if (oldNick != newNick)
+    {
+        std::string userMsg;
+        if (client.isLoggedIn())
+            userMsg = ":" + oldNick + "!" + client.getUserName() + "@" + server_.getIP();
+        else
+            userMsg = ":" + server_.getIP() + " NICK :" + newNick + "\r\n";
+        
+        client.setNickname(newNick);
+
+        std::set<int> notifiedClient;
+        for (const auto& channel : channels)
+        {
+            if (channel.second.isMember(clientFd))
+            {
+                for (int memberFd : channel.second.getMembers())
+                {
+                    if (notifiedClients.insert(memberFd).second)
+                    {
+                        server_.msgClient(memberFd, userMsg);
+                    }
+                }
+            }
+        }
+        if (notifiedClients.find(clientFd) == notifiedCients.end())
+            server_.msgClient(clientFd, userMsg);
+    }
+}
 
 /**
  * ✨ Tips for Siria
--- commands/CommandHandler.hpp
+++ commands/CommandHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 00:33:34 by eahn              #+#    #+#             */
-/*   Updated: 2025/04/17 13:27:49 by smiranda         ###   ########.fr       */
+/*   Updated: 2025/04/17 15:29:44 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 #include <functional> 
 #include <regex>
 #include "../utils/InputParser.hpp" // for ParsedCommand
+#include "../utils/Macros.hpp" // for error messages
+#include "../core/Server.hpp" // communication with client
 
 class CommandHandler
 {
@@ -28,6 +30,7 @@ class CommandHandler
 		void dispatch(int clientFd, const ParsedCommand& command);
 
 	private:
+		Server& server_;
 		// command string -> handler function
     	typedef void (CommandHandler::*CommandFunc)(int, const std::vector<std::string>&);
     	std::map<std::string, std::function<void(int, const std::vector<std::string>&)> > commandTable;
